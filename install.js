6208102c2ea94fb5788dab2339c69613d08560f5
cocktailpeanut <cocktailpeanuts@proton.me>
2024-10-23 19:12:59 -0400

    update

--- install.js
+++ install.js
@@ -25,6 +25,15 @@ module.exports = {
     {
       method: "shell.run",
       params: {
+        message: [
+          "conda install -y -c conda-forge ninja"
+        ]
+      }
+    },
+    // Edit this step with your custom install commands
+    {
+      method: "shell.run",
+      params: {
         venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
