d48e11207090f14ffd464b8d76083a6917539cad
cocktailpeanut <cocktailpeanuts@proton.me>
2024-10-23 21:20:51 -0400

    update

--- install.js
+++ install.js
@@ -21,23 +21,40 @@ module.exports = {
         }
       }
     },
+//    // Edit this step with your custom install commands
+//    {
+//      method: "shell.run",
+//      params: {
+//        message: [
+//          "conda install -y -c conda-forge ninja"
+//        ]
+//      }
+//    },
     // Edit this step with your custom install commands
     {
+      when: "{{platform === 'win32' && gpu === 'nvidia'}}",
       method: "shell.run",
       params: {
-        message: [
-          "conda install -y -c conda-forge ninja"
-        ]
+        venv: "env",                // Edit this to customize the venv folder path
+        path: "app",                // Edit this to customize the path to start the shell from
+        message: "pip install https://github.com/oobabooga/flash-attention/releases/download/v2.6.3/flash_attn-2.6.3+cu122torch2.4.1cxx11abiFALSE-cp310-cp310-win_amd64.whl"
       }
     },
-    // Edit this step with your custom install commands
+    {
+      when: "{{platform === 'linux' && gpu === 'nvidia'}}",
+      method: "shell.run",
+      params: {
+        venv: "env",                // Edit this to customize the venv folder path
+        path: "app",                // Edit this to customize the path to start the shell from
+        message: "pip install https://github.com/Dao-AILab/flash-attention/releases/download/v2.6.3/flash_attn-2.6.3+cu123torch2.4cxx11abiFALSE-cp310-cp310-linux_x86_64.whl"
+      }
+    },
     {
       method: "shell.run",
       params: {
         venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
-          "pip install wheel",
           "pip install addict click einops flash-attn gradio devicetorch omegaconf pillow pyyaml ray sentencepiece setuptools transformers"
           //"pip install -e . --no-build-isolation"
           //"pip install -r requirements.txt"
