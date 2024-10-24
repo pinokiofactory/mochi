4c2476580bee3ca54b52a3017659640a4fde9700
cocktailpeanut <cocktailpeanuts@proton.me>
2024-10-23 22:10:35 -0400

    update

--- install.js
+++ install.js
@@ -55,9 +55,9 @@ module.exports = {
         venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
-          "pip install -e ."
+          //"pip install -e ."
           //"pip install addict click einops flash-attn gradio==5.1.0 devicetorch omegaconf pillow pyyaml ray sentencepiece setuptools transformers"
-          //"pip install -e . --no-build-isolation"
+          "pip install -e . --no-build-isolation"
           //"pip install -r requirements.txt"
         ]
       }
