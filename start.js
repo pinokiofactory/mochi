2ebac1c017c53136e5733469b0acd93dab35b43f
cocktailpeanut <cocktailpeanuts@proton.me>
2024-10-23 21:50:58 -0400

    update

--- start.js
+++ start.js
@@ -4,13 +4,17 @@ module.exports = {
     {
       method: "shell.run",
       params: {
-        venv: "../../env",                // Edit this to customize the venv folder path
+        //venv: "../../env",                // Edit this to customize the venv folder path
+        venv: "env",
         env: {
           "TRANSFORMERS_VERBOSITY": "info"
         },                   // Edit this to customize environment variables (see documentation)
-        path: "app/src/mochi_preview",                // Edit this to customize the path to start the shell from
+        //path: "app/src/mochi_preview",                // Edit this to customize the path to start the shell from
+        path: "app",
         message: [
-          "python gradio_ui.py --model_dir ../../checkpoint"
+          //"python gradio_ui.py --model_dir ../../checkpoint"
+          //"python -m mochi_preview.gradio_ui --model_dir ../../checkpoint"
+          "python -m mochi_preview.gradio_ui --model_dir checkpoint"
         ],
         on: [{
           // The regular expression pattern to monitor.
