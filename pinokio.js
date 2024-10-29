93d4b2bddb16f3ce883f47336f1de7c126955d6e
cocktailpeanut <cocktailpeanuts@proton.me>
2024-10-28 23:03:02 -0400

    update

--- pinokio.js
+++ pinokio.js
@@ -5,7 +5,7 @@ module.exports = {
   description: "",
   icon: "icon.png",
   menu: async (kernel, info) => {
-    let installed = info.exists("app/env")
+    let installed = info.exists("app/demos/env")
     let running = {
       install: info.running("install.js"),
       start: info.running("start.js"),
